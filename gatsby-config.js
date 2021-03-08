3598a2f08b2fe9de3e89d3d033ddefa74a15630d
boghsteren <boghsteren@gmail.com>
2021-03-08 17:24:37 +0100

    Removed sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,6 @@ module.exports = {
     },
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
